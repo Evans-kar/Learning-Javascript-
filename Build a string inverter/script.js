e18cce2b07441cd7be44b11d9292ff5ac82aa606
Evans Karuri <duneevanson6@gmail.com>
2026-03-18 07:11:23 -0400

    broke my streak the 18th is day one of 100 days

--- Build a string inverter/script.js
+++ Build a string inverter/script.js
@@ -1,5 +1,5 @@
-function reverseString(str) {
-  return str.split("").reverse().join("");
-}
-
-console.log(reverseString("hello")); // "olleh"
+//my first call back function using the forEachMethod. The callback function takes 3 parameters ie. the element, index, and array.
+let numbers = [1,2,3,4,5];
+numbers.forEach(function(number,index,array){
+  console.log(`element ${number} is at index ${index} in array ${array}`);
+})
